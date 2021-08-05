6d25d44311f1ae4334f788bb270a5abe75f77c1d
Rohied Abdoella <rohiedabdoella@gmail.com>
2021-08-05 20:07:29 +0200

    1e versie van opdracht inleveren

---
+++ script.js
@@ -0,0 +1,23 @@
+const array = [
+    { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+    { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
+    { name: "A. Curry", profession: "kikvorsman", age: 32 },
+    { name: "F. Vonk", profession: "slangenmelker", age: 36 },
+    { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
+    { name: "Dr.Evil", profession: "digital overlord", age: 56 }
+  ];
+  
+  for (let person of array) {
+      if age = > 50  {
+         console.log((person.name), "ouder dan 50");
+  }
+      }
+    //hier moeten console.logs komen
+    //console.log("Alle objecten uit de loop gelogd:", (person));
+    //console.log("Dit is:", (person.name));
+    //console.log("Geboortejaar:", (person.name), (person.age - 2021));
+    //console.log((person.name),  "is een", (person.profession));
+    
+  }
+
+ 
