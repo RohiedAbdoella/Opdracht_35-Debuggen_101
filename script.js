8d3ab032fd7a400921658ac68e12e22949fc58ad
Rohied Abdoella <rohiedabdoella@gmail.com>
2021-08-05 20:14:53 +0200

    herziene versie na nakijken van Winc

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const array = [
   ];
   
   for (let person of array) {
-      if age = > 50  {
+      if (person.age > 50) {
          console.log((person.name), "ouder dan 50");
-  }
+  
       }
     //hier moeten console.logs komen
     //console.log("Alle objecten uit de loop gelogd:", (person));
@@ -20,4 +20,22 @@ const array = [
     
   }
 
- 
+  //Winc oplossing:
+  const array = [
+    { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+    { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
+    { name: "A. Curry", profession: "kikvorsman", age: 32 },
+    { name: "F. Vonk", profession: "slangenmelker", age: 36 },
+    { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
+    { name: "Dr.Evil", profession: "digital overlord", age: 56 }
+  ];
+  
+  for (let person of array) {
+    console.log("Dit is de gehele persoon:", person);
+    console.log("en hier hebben we: ", person.name);
+    console.log("geboortedatum:", 2020 - person.age);
+    console.log("Beroep:", person.name + " is een " + person.profession);
+        if (person.age > 50){
+                console.log("Persoon is ouder dan 50", person.name)
+        }
+  }
